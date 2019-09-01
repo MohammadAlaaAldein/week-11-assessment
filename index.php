64c9ba7deacb4a36832e0e4d450dbbb062e8bd2f
MohammadAlaaAldein <mohammad.aladin996@gmail.com>
2019-09-01 11:01:02 +0300

    done

--- index.php
+++ index.php
@@ -1,9 +1,62 @@
+<?PHP
+    $salary1="700$";
+    $salary2="800$";
+    echo "<table> <tr> <td><b>Salary of Mohammad</b></td> <td>$salary1</td> </tr> <tr> <td><b>Salary of Ayham</b></td> <td>$salary2</td> </tr> </table>";
+?>
+
+
+
 <?php
+$countries = array( "Italy"=>"Rome",
+"Luxembourg"=>"Luxembourg",
+ "Belgium"=> "Brussels",
+ "Denmark"=>"Copenhagen",
+ "Finland"=>"Helsinki",
+ "France" => "Paris",
+ "Slovakia"=>"Bratislava" ) ;
 
-/* Question 1 : Write a PHP script to display names and the salaries amounts, within a table. ou should use `echo ` to generate your table. */
+    ksort($countries);
+ 
+    foreach ($countries as $country => $capital) {
+        echo "<ul><li>The Country is $country and the capital is $capital</li></ul>";
+    }
+?>
+
+<?php
+    $star="*";
+    for ($i=0; $i<5; $i++){
+        echo "$star <br>";
+        $star = $star . "*";
+    }
+
+?>
+
+
+
+<?php
+    $datetime1 = new DateTime('1981-11-04');
+    $datetime2 = new DateTime('2013-09-04');
+    $interval = $datetime1->diff($datetime2);
+    echo $interval->format('%y Year %m Month %d Day'  );
+?>
+
+
+
+    <form method="post" action="">
+        <input type="text"  placeholder="write here" name="text"/>
+        <input type="submit" onclick="write()" value="Submit" name="submit "/>
+    </form>     
+
+<?php 
+    $text_inputed=$_POST["text"];
+    function write(){
+        echo $text_inputed;
+    }
+?>
+
+
+<!-- /* Question 1 : Write a PHP script to display names and the salaries amounts, within a table. ou should use `echo ` to generate your table. */
 //////////////////////// Your Code Here /////////////////////
-
-
 
 
 
@@ -35,7 +88,8 @@ $countries = array( "Italy"=>"Rome",
 
 /*Question 4 : Write a PHP script to calculate the difference between two dates.
 Input : 1981-11-04, 2013-09-04
-Output : 31 years, 10 months, 11 days
+Output : 31 years, 10 months, 0 days
 
 
 /*Question 5 : Create a simple HTML form that accept the user name after submitting display the name using PHP echo statement under the form. */
+?> -->
